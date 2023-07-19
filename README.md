ed05d0014692ef107edd19e38ed693e5d2f4617e
DanielT504 <62156098+DanielT504@users.noreply.github.com>
2023-07-18 22:47:50 -0400

    Added 3rd dimension and UI update

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Particle-Swarm-Sphere-Optimizer
+
+This is a Particle Swarm Optimization (PSO) based on the social behavior of bird flocking and fish schooling. The algorithm starts with a swarm of particles that iteratively explore and exploit the search space to find the optimal solution for the sphere function (3-dimensional in this demonstration). The particles update their velocities and positions based on their own best-known position and the global best position found by the swarm. 
+
 ![chess-bot](https://github.com/DanielT504/Particle-Swarm-Sphere-Optimizer/assets/62156098/470380df-03f2-404b-9f4d-41eff901ddc8)
 
 Global Best Position: [4.0612158514502505e-05, -0.00015319389628730418]
--- particle_swarm.py
+++ particle_swarm.py
@@ -1,17 +1,20 @@
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Constants
 NUM_PARTICLES = 200
-NUM_DIMENSIONS = 4
-MAX_ITERATIONS = 200
+NUM_DIMENSIONS = 2
+MAX_ITERATIONS = 50
 C1 = 2.0  # Cognitive parameter
 C2 = 2.0  # Social parameter
 W = 0.7   # Inertia weight
 
 # Function to optimize (example: sphere function)
 def fitness_function(position):
-    return sum([x**2 for x in position])
+    x = position[0]
+    y = position[1]
+    return x**2 + y**2
 
 class Particle:
     def __init__(self):
@@ -51,9 +54,24 @@ ax.set_xlim(-6, 6)
 ax.set_ylim(-6, 6)
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
+
+# Generate a grid of points to evaluate the fitness function
+x_vals = np.linspace(-6, 6, 100)
+y_vals = np.linspace(-6, 6, 100)
+X, Y = np.meshgrid(x_vals, y_vals)
+Z = fitness_function([X, Y])
+
+# Create a color map to represent the fitness values
+cmap = plt.cm.get_cmap('jet')
+norm = plt.Normalize(vmin=np.min(Z), vmax=np.max(Z))
+
+# Plot the fitness landscape
+ax.imshow(Z, extent=(-6, 6, -6, 6), origin='lower', cmap=cmap, norm=norm)
+
+# Initialize the particles on the plot
 particles_scatter = ax.scatter([particle.position[0] for particle in particles],
                               [particle.position[1] for particle in particles],
-                              color='b', marker='o')
+                              color='white', marker='o', s=10)
 
 # PSO main loop
 iteration = 0
@@ -69,7 +87,7 @@ while iteration < MAX_ITERATIONS:
 
     # Update particle positions on the plot
     particles_scatter.set_offsets([[particle.position[0], particle.position[1]] for particle in particles])
-    plt.pause(0.05)
+    plt.pause(0.2)
 
     iteration += 1
 
